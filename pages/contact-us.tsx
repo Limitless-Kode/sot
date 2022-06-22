94ba747b8fd80484d990d39927cf8a40ce7baa68
Peter Claver <limitless.claver@gmail.com>
2022-06-22 05:52:36 +0000

    implemented contact form with emailjs

--- pages/contact-us.tsx
+++ pages/contact-us.tsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useRef, useState} from 'react'
 import { NextPage } from 'next';
 import Nav from '../src/components/navigation';
 import styles from '../styles/Home.module.css';
@@ -8,9 +8,13 @@ import Head from 'next/head';
 import Circle from '../src/components/circle';
 import Footer from '../src/sections/footer'
 import { FiUser, FiMail, FiPhone } from 'react-icons/fi';
+import emailjs from '@emailjs/browser';
+  import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
 
 const ContactUs : NextPage = () => {
-  const [value, setValue] = useState('');
+  const [formDisabled, setFormDisabled] = useState(false);
+  const formRef = useRef<HTMLFormElement>(null);
   const form = useForm({
     initialValues: {
       name:'',
@@ -23,9 +27,34 @@ const ContactUs : NextPage = () => {
       name: (value) => (value.trim().length < 2 ? 'Name must have at least 2 letters' : null),
       email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid Email Address'),
       phone: (value) => (/(\+\d{1,3}\s?)?((\(\d{3}\)\s?)|(\d{3})(\s|-?))(\d{3}(\s|-?))(\d{4})(\s?(([E|e]xt[:|.|]?)|x|X)(\s?\d+))?/.test(value) ? null : 'Please Enter a Valid Phone Number'),
-      message: (value) => (value.trim().length < 10 ? null : 'Message must be more than 10 characters.'),
+      message: (value) => (value.trim().length > 10 ? null : 'Message must be more than 10 characters.'),
     },
   });
+
+
+  const sendEmail = () => {
+    setFormDisabled(true);
+    const id = toast.loading('Sending Message');
+
+    emailjs.sendForm('service_2ssz86k', 'template_d1rvez8', formRef.current, 'user_2RR2joOVprhH8K4kCh16P')
+      .then((result) => {
+          toast.update(id, {
+            render: 'Message has been sent successfully.',
+            type: 'success',
+            isLoading: false,
+            autoClose: 3000,
+          });
+        formRef?.current.reset();
+      }, (error) => {
+          toast.update(id, {
+            render: 'Something wrong happened. Please try again.',
+            type: 'error',
+            isLoading: false,
+            autoClose: 3000,
+          });
+      });
+      setFormDisabled(false);
+  };
   return (
     <div>
 			<Head>
@@ -36,6 +65,7 @@ const ContactUs : NextPage = () => {
 
 			<main className={styles.main}>
 				<Nav />
+        <ToastContainer />
         <div className="relative h-[30vh] xl:h-[50vh] w-[100vw] py-16 bg-gradient-to-r from-primary to-secondary flex justify-center items-center">
           <Circle className="absolute w-[35vw] h-[35vw] md:h-[20vw] md:w-[20vw] rounded-full md:left-[-10vw] left-[-20vw]" />
 
@@ -48,11 +78,12 @@ const ContactUs : NextPage = () => {
         </div>
 
         <div className='w-[80vw] lg:w-[40vw] py-16'>
-          <form onSubmit={form.onSubmit((values) => console.log(values))}>
+          <form ref={formRef} onSubmit={form.onSubmit(sendEmail)}>
             <p className='mb-8'>Please leave us a message and we will get back to you shortly.</p>
-            <TextInput icon={<FiUser />} placeholder="Full Name" styles={{input: {fontSize: 16}, error: {fontSize:14, marginLeft: 25}}} radius="xl" size="xl" {...form.getInputProps('name')}/>
+            <TextInput name='name' icon={<FiUser />} placeholder="Full Name" styles={{input: {fontSize: 16}, error: {fontSize:14, marginLeft: 25}}} radius="xl" size="xl" {...form.getInputProps('name')}/>
           <div className="flex flex-col gap-5 my-5">
               <TextInput
+                name='email'
                 icon={<FiMail />} 
                 placeholder="Email Address" 
                 styles={{ input: { fontSize: 16 }, error: {fontSize:14, marginLeft: 25} }}
@@ -60,9 +91,10 @@ const ContactUs : NextPage = () => {
                 size="xl" 
                 {...form.getInputProps('email')}
               />
-            <TextInput icon={<FiPhone />} placeholder="Phone Number" styles={{input: {fontSize: 16}, error: {fontSize:14, marginLeft: 25}}} radius="xl" size="xl" {...form.getInputProps('phone')}/>
+            <TextInput name='phone' icon={<FiPhone />} placeholder="Phone Number" styles={{input: {fontSize: 16}, error: {fontSize:14, marginLeft: 25}}} radius="xl" size="xl" {...form.getInputProps('phone')}/>
           </div>
-          <Textarea
+            <Textarea
+            name='message'
             placeholder="Your Message"
             autosize 
             minRows={6} 
@@ -73,7 +105,7 @@ const ContactUs : NextPage = () => {
             {...form.getInputProps('message')}
             />
           <div className="flex justify-end my-8">
-            <Button type="submit" radius="xl" size="xl" className='bg-secondary text-sm uppercase hover:bg-secondary'>
+            <Button disabled={formDisabled} type="submit" radius="xl" size="xl" className='bg-secondary text-sm uppercase hover:bg-secondary'>
             Send Message
           </Button>
           </div>
