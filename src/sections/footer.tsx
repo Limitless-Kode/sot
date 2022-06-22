146de932028a21f62124ee7b993a6203588cc9fb
Peter Claver <limitless.claver@gmail.com>
2022-06-22 05:56:33 +0000

    added link to footer contact form

--- src/sections/footer.tsx
+++ src/sections/footer.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
 import { Button, Text } from '@mantine/core';
 import IconButton from '../components/icon_button';
+import Link from 'next/link';
 
 
 const Footer = () => {
@@ -10,9 +11,11 @@ const Footer = () => {
 				<div className='bg-primary w-[100vw] py-16 text-white flex flex-col items-center justify-center'>
 					<h2 className="text-2xl lg:text-3xl font-semibold mb-3">Join us make impact</h2>
 					<Text className="text-sm lg:text-lg">Volunteer with us today and build your community </Text>
-					<Button radius="xl" size="lg" className='mt-8 bg-white text-primary text-xs uppercase hover:bg-secondary'>
-						Contact Us
-					</Button>
+					<Link href='/contact-us'>
+						<Button radius="xl" size="lg" className='mt-8 bg-white text-primary text-xs uppercase hover:bg-secondary'>
+							Contact Us
+						</Button>
+          </Link>
 				</div>
 				<div className="flex items-center justify-center gap-5 text-white p-8">
 					<IconButton
